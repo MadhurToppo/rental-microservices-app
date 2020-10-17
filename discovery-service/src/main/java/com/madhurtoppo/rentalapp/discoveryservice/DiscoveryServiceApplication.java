3474da1c8a66b8879aba8004cc2cc5a5a2e9c49b
MadhurToppo <madhur.toppo@gmail.com>
2020-10-17 09:24:50 +0530

    Added author details and formatted code

--- discovery-service/src/main/java/com/madhurtoppo/rentalapp/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/madhurtoppo/rentalapp/discoveryservice/DiscoveryServiceApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/***
+ * @author Madhur Toppo
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
 
 }
