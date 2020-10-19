04d79ca0780a0fd118503c9df67e646ad1e692d9
MadhurToppo <madhur.toppo@gmail.com>
2020-10-19 09:38:46 +0530

    Added customer service version

--- customer-service/src/main/java/com/madhurtoppo/rentalapp/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/madhurtoppo/rentalapp/customerservice/CustomerServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /***
+ * Customer service for customer information
  * @author Madhur Toppo
+ * @version 1.0
+ * @since 28 March 2020
  */
 @SpringBootApplication
 @EntityScan(basePackages = "com.madhurtoppo.rentalcommons.model.customer")
