96002b07042d484c71add22ee25ed1d3b30290a8
MadhurToppo <madhur.toppo@gmail.com>
2020-10-22 20:55:30 +0530

    Added version and date

--- authorization-server/src/main/java/com/madhurtoppo/rentalapp/authorizationserver/repository/UserDetailRepository.java
+++ authorization-server/src/main/java/com/madhurtoppo/rentalapp/authorizationserver/repository/UserDetailRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 /***
  * @author Madhur Toppo
+ * @version 1.0
+ * @since 28 March 2020
  */
 public interface UserDetailRepository extends JpaRepository<User, Integer> {
 
