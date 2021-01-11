315ce4b67ff027c32b2b34522268ae5a4115d331
madhurtoppo <Madhur0928>
2021-01-12 02:23:23 +0530

    Formatting code

--- rental-ui/src/main/java/com/madhurtoppo/rentalapp/rentalui/RentalUiApplication.java
+++ rental-ui/src/main/java/com/madhurtoppo/rentalapp/rentalui/RentalUiApplication.java
@@ -3,7 +3,6 @@ package com.madhurtoppo.rentalapp.rentalui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /***
  * Rent UI Client for Products Rental Application
