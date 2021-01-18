d91d2e9a1661103fdfedfc88cabeef51a5178c17
madhurtoppo <madhur.toppo@gmail.com>
2021-01-18 23:02:34 +0530

    Added date

--- rent-service/src/main/java/com/madhurtoppo/rentalapp/rentservice/repository/RentRepository.java
+++ rent-service/src/main/java/com/madhurtoppo/rentalapp/rentservice/repository/RentRepository.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 /***
  * @author Madhur Toppo
- * @version 1.0
+ * @since 28 March 2020
  */
 public interface RentRepository extends JpaRepository<Rent, Integer> {
     Integer countByProductId(Integer productId);
