79215a8dfe75602d9a1ffeb25580510ed63f09ab
madhurtoppo <madhur.toppo@gmail.com>
2021-02-11 10:22:07 +0530

    Changed date

--- product-service/src/main/java/com/madhurtoppo/rentalapp/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/madhurtoppo/rentalapp/productservice/ProductServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  * Product service for camera models and accessories
  * @author Madhur Toppo
  * @version 1.0
- * @since 28 March 2020
+ * @since 15 March 2020
  */
 @SpringBootApplication
 @EntityScan(basePackages = "com.madhurtoppo.rentalcommons.model.product")
