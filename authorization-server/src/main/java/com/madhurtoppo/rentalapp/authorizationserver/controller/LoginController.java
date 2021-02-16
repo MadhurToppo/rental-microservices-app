3e2a47b4d5a430d87d671bc9f0812ab0c48aae6f
madhurtoppo <madhur.toppo@gmail.com>
2021-02-16 18:45:54 +0530

    Removed version

--- authorization-server/src/main/java/com/madhurtoppo/rentalapp/authorizationserver/controller/LoginController.java
+++ authorization-server/src/main/java/com/madhurtoppo/rentalapp/authorizationserver/controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /***
  * @author Madhur Toppo
  * @since 15 March 2020
- * @version 1.0
  */
 @Controller
 public class LoginController {
