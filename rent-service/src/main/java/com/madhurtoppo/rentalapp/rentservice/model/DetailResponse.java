b7ae6ebed30b54e8975c8faf51134cb6eed23edf
madhurtoppo <madhur.toppo@gmail.com>
2021-02-24 23:43:51 +0530

    Updated date

--- rent-service/src/main/java/com/madhurtoppo/rentalapp/rentservice/model/DetailResponse.java
+++ rent-service/src/main/java/com/madhurtoppo/rentalapp/rentservice/model/DetailResponse.java
@@ -6,7 +6,7 @@ import com.madhurtoppo.rentalcommons.model.rent.Rent;
 
 /***
  * @author Madhur Toppo
- * @since 28 March 2020
+ * @since 15 March 2020
  */
 public class DetailResponse implements Response {
 
