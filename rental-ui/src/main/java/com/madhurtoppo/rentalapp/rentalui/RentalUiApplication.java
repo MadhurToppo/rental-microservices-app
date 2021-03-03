a1fe9175602f30bd9f8123e1caf49ef87f68ff49
madhurtoppo <madhur.toppo@gmail.com>
2021-03-03 21:10:57 +0530

    Updated date

--- rental-ui/src/main/java/com/madhurtoppo/rentalapp/rentalui/RentalUiApplication.java
+++ rental-ui/src/main/java/com/madhurtoppo/rentalapp/rentalui/RentalUiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Rent UI Client for Products Rental Application
  * @author Madhur Toppo
  * @version 1.0
- * @since 28-March-2020
+ * @since 15-March-2020
  *
  */
 @SpringBootApplication
